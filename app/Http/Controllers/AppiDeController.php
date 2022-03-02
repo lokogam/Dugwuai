23223178fa8c33a0f3462845addca397273c5ff2
DUVAN <duvangamboa8@gmail.com>
2022-03-02 12:31:14 -0500

    API  PAGINACION

--- app/Http/Controllers/AppiDeController.php
+++ app/Http/Controllers/AppiDeController.php
@@ -11,21 +11,42 @@ class AppiDeController extends Controller
     use WithPagination;
     protected $paginationTheme = 'booststrap';
 
-    public function index()
+    public function index(Request $request)
     {
-        $valI = 0;
-        $valS = 5;
-
+        $valIn  = $request->input('valI');
         $url = 'https://www.datos.gov.co/resource/rpmr-utcd.json';
-        $paginacion = '?$limit='.$valS.'&$offset='.$valI.'&$order=nit_de_la_entidad';
-        $datos = Http::get($url.$paginacion);
-        $api= $datos->json();
+        $valS = 10;
 
-        return view('rol/api/index',compact('api') );
+        
+        if($request->input('paginate') && $request->input('paginate') == 2){
+            $valI = $valIn;
+            $paginacion = '?$limit='.$valS.'&$offset='.$valI.'&$order=nit_de_la_entidad';
+            $datos = Http::get($url.$paginacion);
+            $api= json_decode($datos);
+            return view('rol/api/table',compact('api','valI', 'valS') );
+
+        }else if($request->input('paginate') && $request->input('paginate') == 1){
+            $valI =$valIn;
+            $paginacion = '?$limit='.$valS.'&$offset='.$valI.'&$order=nit_de_la_entidad';
+            $datos = Http::get($url.$paginacion);
+            $api= json_decode($datos);
+            return view('rol/api/table',compact('api','valI', 'valS') );
+
+        }else{
+            $valI =0;
+            $paginacion = '?$limit='.$valS.'&$offset='.$valI.'&$order=nit_de_la_entidad';
+            $datos = Http::get($url.$paginacion);
+            $api= json_decode($datos);
+
+            return view('rol/api/index',compact('api','valI', 'valS') );
+        }
+
         // return response()->json($api);
     }
 }
 
+// print_r($api);
+// exit();
 // calve id
 // 4z8u72ac36h27vbkgjfvvz5hg
 
--- resources/views/rol/api/index.blade.php
+++ resources/views/rol/api/index.blade.php
@@ -11,49 +11,14 @@
                                 <div class="card-header">
                                     <h5 class="card-title mb-0">Usuarios registrados</h5>
                                 </div>
-                                <div class="card-body ">
-                                    <table class="table table-striped table-hover">
-                                        <thead>
-                                            <tr>
-                                                <th class="">Nivel Entidad</th>
-                                                <th class="">Nombre de entidad</th>
-                                                <th class="">Nit de la entidad</th>
-                                                <th class="">Estado del proceso</th>
-                                                <th class="">Modalidad de contracion</th>
-                                                <th class="">Objeto a contratar</th>
-                                                <th class="">Tipo de contrato</th>
-                                                <th class="">fecha de firma de contrato</th>
-                                                <th class="">Numero de contrato</th>
-                                                <th class="">Numero de proceso</th>
-                                                <th class="">Valor contrato</th>
-                                                <th class="">Non raz Social contratista</th>
-                                                <th class="">URL</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($api as $i)
-                                            <tr>
-                                                <td>{{ $i['nivel_entidad'] }}</td>
-                                                <td>{{ $i['nombre_de_la_entidad'] }}</td>
-                                                <td>{{ $i['nit_de_la_entidad'] }}</td>
-                                                <td>{{ $i['estado_del_proceso'] }}</td>
-                                                <td>{{ $i['modalidad_de_contrataci_n'] }}</td>
-                                                <td>{{ $i['objeto_a_contratar'] }}</td>
-                                                <td>{{ $i['tipo_de_contrato'] }}</td>
-                                                <td>fecha_de_firma_del_contrato</td>
-                                                <td>{{ $i['numero_del_contrato'] }}</td>
-                                                <td>{{ $i['numero_de_proceso'] }}</td>
-                                                <td>{{ $i['valor_contrato'] }}</td>
-                                                <td>{{ $i['nom_raz_social_contratista'] }}</td>
-                                                <td>{{ $i['url_contrato'] }}</td>
-                                            </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
+                                <div class="card-body " id="tableApi">
+                                    @include('rol.api.table')
                                 </div>
                                 <div class="card-footer">
-                                    <button class="btn btn-secondary" id="anterior" onchange="pagination()" >Anterior</button>
-                                    <button class="btn btn-secondary" id="sigiente" onchange="pagination()">Sigiente</button>
+                                    <input type="hidden" value="{{ $valI}}" id="paginationI"  >
+                                    <input type="hidden" value="{{ $valS}}" id="paginationS"  >
+                                    <button class="btn btn-secondary" onclick="paginationA()" >Anterior</button>
+                                    <button class="btn btn-secondary"  onclick="paginationS()">Sigiente</button>
                                 </div>
                             </div>
                         </div>
@@ -63,8 +28,49 @@
         </div>
 </main>
 <script>
-    function pagination(){
+    function paginationA(){
+        var pagI = $("#paginationI").val();
+        var pagS = $("#paginationS").val();
+        pagI = pagI - pagS;
+        console.log( pagI);
+
+        var parametros = {
+                    "valIn": pagI,
+                    "paginate": 1
+                };
+
+            $.ajax({
+                data: parametros,
+                url: " {{ route('appi') }}",
+                type: 'get',
+
+                success: function(response) {
+                    $("#tableApi").html(response);
+                }
+            });
         
+    }
+
+    function paginationS(){
+        var pagI = $("#paginationI").val();
+        var pagS = $("#paginationS").val();
+        pagI = pagI + pagS;
+        console.log( pagI);
+
+        var parametros = {
+                    "valIn": pagI,
+                    "paginate": 2
+                };
+
+            $.ajax({
+                data: parametros,
+                url: " {{ route('appi') }}",
+                type: 'get',
+
+                success: function(response) {
+                    $("#tableApi").html(response);
+                }
+            });
     }
 </script>
 @include('layouts.footer')
